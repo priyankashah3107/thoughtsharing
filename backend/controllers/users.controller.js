be5b673cfacb0c2f50cbea2665f8e5570e49d0bd
Priyanka <priyankashah3107@gmail.com>
2024-06-15 02:32:36 +0530

    followUnfollowRoute

--- backend/controllers/users.controller.js
+++ backend/controllers/users.controller.js
@@ -25,10 +25,37 @@ export const suggestUser = async (req, res) => {
 
 
 export const followUnFollowUser = async (req, res) => {
+  const {id} = req.params;
   try {
+    const userModify = await User.findById(id);
+    const currentUser = await User.findById(req.user._id)
+
+    if(id === req.user._id.toString()) {
+      return res.status(400).json({error: "You cannot follow or unfollow yourself"})
+    }
     
+    if(!userModify || !currentUser) {
+      return res.status(404).json({error: "User not Found"})
+
+    }
+
+    const isFollowing = await currentUser.following.includes(id)
+     
+    if(isFollowing) {
+      // Unfollow the user 
+      await User.findByIdAndUpdate(id, {$pull: {followers: req.user._id}})
+      await User.findByIdAndUpdate(req.user._id, {$pull: {following: id}})
+      res.status(200).json({msg: "Unfollowed Uncessfully!"})
+    } 
+    else {
+      // follow the user
+      await User.findByIdAndUpdate(id, {$push: {followers: req.user._id}})
+      await User.findByIdAndUpdate(req.user._id, {$push: {following: id}})
+      res.status(200).json({msg: "User Followed Successfully"})
+    }
   } catch (error) {
-    
+    console.error("Error in FollowUnFollowUser Controller", error.message)
+    res.status(500).json({error: "Internal Server Error"})
   }
 }
 
--- backend/routes/users.routes.js
+++ backend/routes/users.routes.js
@@ -6,7 +6,7 @@ const router = Router();
 
 router.get("/profile/:username", protectRoute, getUserProfile);
 // router.get("/suggest", protectRoute, suggestUser);
-// router.post("/follow/:id", protectRoute, followUnFollowUser);
+router.post("/follow/:id", protectRoute, followUnFollowUser);
 // router.post("/update", protectRoute, updateUser)
 
 
