4136f3c31b047346e2df1e8372d5520640bab68b
Priyanka <priyankashah3107@gmail.com>
2024-06-23 16:18:25 +0530

    err: InDeletePost

--- backend/controllers/post.controller.js
+++ backend/controllers/post.controller.js
@@ -1,8 +1,36 @@
+import User from "../models/user.model.js"
+import Post from "../models/post.model.js"
+import {v2 as cloudinary} from 'cloudinary';
 export const createPost = async(req, res) => {
  try {
-  
+  const {text} = req.body;
+  let {img} = req.body;
+  const userId = req.user._id.toString();
+
+  const user = await User.findById(userId)
+
+  if(!user) return res.status(404).json({error: "User not found"});
+
+  if(!text && !img) {
+     return res.status(400).json({error: "Post Must have contain text and image"})
+  }
+   
+  if(img) {
+    const responseUploaded = await cloudinary.uploader.upload(img)
+    img = responseUploaded.secure_url;
+  }
+ 
+  const newPost  = new Post({
+     user: userId,
+     text,
+     img,
+  })
+  await newPost.save();
+  res.status(200).json({newPost})
+
  } catch (error) {
-  
+   res.status(500).json({error: "Internal server error"});
+   console.error("Error in post controllerJs ", error)
  }
 }
 
@@ -11,7 +39,11 @@ export const createPost = async(req, res) => {
 
 export const likeOrUnLikePost = async(req, res) => {
   try {
-   
+    const userId = req.params.id;
+
+    const user = await User.findById(userId)
+
+
   } catch (error) {
    
   }
@@ -30,8 +62,22 @@ export const likeOrUnLikePost = async(req, res) => {
 
  export const deletePost = async(req, res) => {
   try {
-   
+    const post = await User.findById(req.params.id);
+    if(!post) return res.status(404).json({error: "Post is not found!"});
+
+    if(post.user.toString() !== req.user._id.toString()) {
+      return res.status(401).json({error: "You are not authorized to delete this post"})
+    } 
+
+    if(post.img) {
+      const imgId = post.img.split("/").pop().split(".")[0];
+      await cloudinary.uploader.destroy(imgId)
+    }
+
+    await Post.findByIdAndDelete(req.params.id);
+    res.status(200).json({message: "Post Deleted successfully"})
   } catch (error) {
-   
+     res.status(500).json({error: "Internal Server Error"})
+     console.error("Error in DeletePost Controller", error)
   }
  }
--- backend/routes/post.routes.js
+++ backend/routes/post.routes.js
@@ -8,8 +8,8 @@ const router  = Router()
 // a) create a post b) delete Post c) Like Post d) comment post e) bookmark the post f) share the post(whatapp or any socialmedia)
 
 router.post("/create", protectRoute, createPost);
-// router.post("/like/:id", protectRoute, likeOrUnLikePost);
+router.post("/like/:id", protectRoute, likeOrUnLikePost);
 // router.post("/comment/:id", protectRoute, commentOnPost);
-// router.delete("/delete", protectRoute, deletePost)
+router.delete("/:id", protectRoute, deletePost)
 
 export default router;
