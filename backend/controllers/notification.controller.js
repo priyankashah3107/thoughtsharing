0af28a42e33f63e8688d075fd0ec66d481abeb40
Priyanka <priyankashah3107@gmail.com>
2024-06-26 21:35:26 +0530

    Notification and routers

--- backend/controllers/notification.controller.js
+++ backend/controllers/notification.controller.js
@@ -26,17 +26,33 @@ export const getNotifications = async (req, res) => {
 export const deleteNotifications = async (req, res) => {
    
   try {
-    
+    const userId = req.user._id;
+
+     await Notification.deleteMany({ to: userId})
+
+     res.status(200).json({message: "all notifications are deleted successfully"})
+
   } catch (error) {
-    
+    console.error("Error in DeleteNotification ", error);
+    res.status(500).json({error: "Internal Server Error"})
   }
 }
 
 
 export const deleteNotificationById = async (req, res) => {
   try {
-    
+     const notificationId  = req.params.id
+     const userId =  req.user._id
+     const notification = await Notification.findById(notificationId)
+
+     if(!notification) return res.status(404).json({error: "Notification not found"})
+
+     if(notification.to.toString() !== userId.toString()) return res.status(403).json({error: "You are not allowed to deleted the  notification of this post"});
+
+     await Notification.findByIdAndDelete(notificationId)
+     res.status(200).json({message: "notification deleted Successfully"})
   } catch (error) {
-    
+    console.error("Error in DeleteNotification ", error);
+    res.status(500).json({error: "Internal Server Error"})
   }
 }
