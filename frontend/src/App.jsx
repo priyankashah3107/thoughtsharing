36337d0fc672e28376c3226281eb79ba2c95cad7
Priyanka <priyankashah3107@gmail.com>
2024-06-28 01:25:59 +0530

    SignUpPage

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,8 +1,20 @@
 import React from 'react'
+import { Route, Routes } from 'react-router-dom'
+import HomePage from './pages/home/HomePage'
+import SignUpPage from './pages/auth/signup/SignUpPage'
+import LoginPage from './pages/auth/login/LoginPage'
 
 const App = () => {
   return (
-    <div>App</div>
+     <> 
+      <div className='flex max-w-6xl mx-auto'>
+			<Routes>
+				<Route path='/' element={<HomePage />} />
+				<Route path='/signup' element={<SignUpPage />} />
+				<Route path='/login' element={<LoginPage />} />
+			</Routes>
+		</div>
+     </>
   )
 }
 
